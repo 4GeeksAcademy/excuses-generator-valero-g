edb9b7f4c7f2b450a992d7796aafedbc072905a7
valero-g <vgaudo@hotmail.com>
2025-03-17 21:46:29 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-import "bootstrap";
-import "./style.css";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let excuse = document.getElementById("excuse");
+  excuse.innerHTML = "<p>" + getExcuse(who) + " " + getExcuse(action) + " " + getExcuse(what) + " " + getExcuse(when) + "</p>";
+}
+
+function getExcuse(array){
+  return array[Math.floor(Math.random()*(array.length))];
+}
